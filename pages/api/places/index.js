ed89ffa39b863f2d49e22c9c158130b946286cb9
Florian Lutz <florian.lutz@ymail.com>
2024-04-09 11:57:40 +0200

    add back and frontend add place

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,8 +5,20 @@ export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
     const places = await Place.find();
-    return response.status(200).json(places);
-  } else {
-    return response.status(400).json({ message: "Bad request" });
+    if (places) {
+      return response.status(200).json(places);
+    } else {
+      return response.status(400).json({ message: "Bad request" });
+    }
+  }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      return response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
